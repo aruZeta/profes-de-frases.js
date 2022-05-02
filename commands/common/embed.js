c7bedad31cc7f4b5fbfebc20f2838b5ff74d6f7b
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-02 21:25:55 +0200

    [refactor] Less indent level for function exports

--- commands/common/embed.js
+++ commands/common/embed.js
@@ -1,11 +1,9 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = {
-	execute(config) {
-		return new MessageEmbed()
-			.setColor('#3498db')
-			.setThumbnail(config.userPfp)
-			.setTimestamp()
-			.setFooter({ text: 'Cristotractor go brrrr', iconURL: config.userPfp });
-	}
+module.exports.execute = (config) => {
+	return new MessageEmbed()
+		.setColor('#3498db')
+		.setThumbnail(config.userPfp)
+		.setTimestamp()
+		.setFooter({ text: 'Cristotractor go brrrr', iconURL: config.userPfp });
 };
