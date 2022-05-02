3fcd6bd715fa46d8ee09c68bc6741f915067045b
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-02 17:49:27 +0200

    [test] Since I removed the web worker this should not be necessary

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const { Client, Collection, Intents } = require('discord.js');
 const config = require('./config.json');
 require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`)
-	const pingLoop = setInterval(() => {
-		console.log('Ping!');
-	}, 60000);
-})
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
