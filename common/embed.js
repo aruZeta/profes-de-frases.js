ec55dd58a793a6ee877cd8300f8603bbd90e5ffa
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-07 10:35:17 +0200

    [add] Change embed color

--- common/embed.js
+++ common/embed.js
@@ -5,7 +5,7 @@ const { userPfp } = require('../config.json');
 module.exports = {
 	embed() {
 		return new MessageEmbed()
-			.setColor('#3498db')
+			.setColor('#1abc9c')
 			.setThumbnail(userPfp)
 			.setTimestamp()
 			.setFooter({ text: 'Cristotractor go brrrr', iconURL: userPfp });
