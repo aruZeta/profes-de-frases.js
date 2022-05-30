40c85a50b710a60d8ac7bcde6ed831f2adb59a9b
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-30 12:21:27 +0200

    [add] Paginating phrases in 'buscar' with buttons n stuff

--- commands/frase/subcommands/buscar.js
+++ commands/frase/subcommands/buscar.js
@@ -1,3 +1,5 @@
+const { MessageButton, MessageActionRow } = require('discord.js');
+
 const {
     checkLetterFound
     , checkId
@@ -37,7 +39,21 @@ module.exports = {
 
         let title;
         let quantity = 0;
-        let phrases = '';
+        let phrases = [];
+        let actualPage = 0;
+        let actualLength = 0;
+
+        const pagePhrase = text => {
+            actualLength += text.length;
+
+            if (actualLength >= 1024) {
+                phrases.push('');
+                actualLength = text.length;
+                actualPage++;
+            }
+
+            return text;
+        }
 
         if (letter != null) {
             letter = letter.toLowerCase();
@@ -51,7 +67,8 @@ module.exports = {
             for (let i = 0; i < found.phrases.length; i++) {
                 const phrase = found.phrases[i];
                 if (new RegExp(word, "i").test(phrase)) {
-                    phrases += `${i}: ${phrase}\n`;
+                    let text = pagePhrase(`${i}: ${phrase}\n`);
+                    phrases[actualPage] += text;
                     quantity++;
                 }
             }
@@ -72,32 +89,110 @@ module.exports = {
 
             await found.forEach(phrase => {
                 if (phrase.letter != lastLetter) {
-                    phrases += `\nCon la letra \`${phrase.letter}\`:\n`;
+                    let text = pagePhrase(`\nCon la letra \`${phrase.letter}\`:\n`);
+                    phrases[actualPage] += text;
                     lastLetter = phrase.letter;
                 }
 
-                phrases += `${phrase.id}: ${phrase.phrases}\n`;
+                text = pagePhrase(`${phrase.id}: ${phrase.phrases}\n`);
+                phrases[actualPage] += text;
                 quantity++;
             });
         }
 
-        const msgEmbed = embed()
-              .setTitle(title)
-              .addFields(
-                  { name: 'Numero de frases:',
-                    value: `${quantity}`,
-                    inline: true
-                  },
-                  { name: 'Frases:',
-                    value: phrases.length > 0
-                    ? phrases
-                    : 'No hay frases con esa palabra'
-                  }
-              );
+        let embedPage = 0;
 
-        await interaction.reply({
-            embeds: [msgEmbed],
-            ephemeral: false
+        const backId = 'back';
+        const forwardId = 'forward';
+
+        const backButton = new MessageButton({
+            style: 'SECONDARY',
+            label: 'Anterior',
+            emoji: '⬅️',
+            customId: backId
         });
+
+        const forwardButton = new MessageButton({
+            style: 'SECONDARY',
+            label: 'Siguiente',
+            emoji: '➡️',
+            customId: forwardId
+        });
+
+        if (phrases.length > 1) {
+            const msgEmbed = () => {
+                return embed()
+                    .setTitle(title)
+                    .addFields(
+                        { name: 'Numero de frases:',
+                          value: `${quantity}`,
+                          inline: true
+                        },
+                        { name: 'Pagina:',
+                          value: `${embedPage + 1}/${phrases.length}`,
+                          inline: true
+                        },
+                        { name: 'Frases:',
+                          value: phrases.length > 0
+                          ? phrases[embedPage]
+                          : 'No hay frases con esa palabra'
+                        }
+                    );
+            }
+
+            await interaction.reply('Resultado:',);
+            
+            const embedReply =
+                  await interaction.channel.send({
+                      embeds: [msgEmbed()],
+                      components: [
+                          new MessageActionRow(
+                              { components: [
+                                  forwardButton
+                              ]}
+                          )
+                      ],
+                      ephemeral: false
+                  });
+            
+            const collector = embedReply.createMessageComponentCollector({
+                componentType: 'BUTTON'
+            });
+            
+            collector.on('collect', async interaction => {
+                interaction.customId === backId ? (embedPage--) : (embedPage++);
+                await interaction.update({
+                    embeds: [msgEmbed()],
+                    components: [
+                        new MessageActionRow(
+                            { components: [
+                                ...(embedPage == 0 ? [] : [backButton]),
+                                ...(embedPage == phrases.length - 1 ? [] : [forwardButton])
+                            ]}
+                        )
+                    ],
+                    ephemeral: false
+                });
+            });
+        } else {
+            await interaction.reply({
+                embeds: [
+                    embed()
+                        .setTitle(title)
+                        .addFields(
+                            { name: 'Numero de frases:',
+                              value: `${quantity}`,
+                              inline: true
+                            },
+                            { name: 'Frases:',
+                              value: phrases.length > 0
+                              ? phrases[0]
+                              : 'No hay frases con esa palabra'
+                            }
+                        )
+                ],
+                ephemeral: false
+            });
+        }
     }
 }
